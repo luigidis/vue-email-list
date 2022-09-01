69d7ca217162e388507717194c62b17a90ef4ff9
LuigiDiS <luigi.distefano69@gmail.com>
2022-09-01 16:30:45 +0200

    Generated 10 Email in Mounted

--- js/app.js
+++ js/app.js
@@ -3,15 +3,17 @@ console.log('yo');
 const app = new Vue ({
     el: '#app',
     data: {
-        randomEmail: '',
+        randomEmail: [],
     },
     mounted() {
+        for (let i = 0; i < 10; i++)
         axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
                 console.log(response)
                 console.log(response.data.response)
-                this.randomEmail = response.data.response
+                this.randomEmail.push(response.data.response)
+                console.log(this.randomEmail)
             })
     },
 })
