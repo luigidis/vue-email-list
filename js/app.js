2770a47e9cc45c32e9c2838772ab30fb985d265b
LuigiDiS <luigi.distefano69@gmail.com>
2022-09-01 16:41:43 +0200

    Problema su beforeMount

--- js/app.js
+++ js/app.js
@@ -5,15 +5,16 @@ const app = new Vue ({
     data: {
         randomEmail: [],
     },
-    mounted() {
-        for (let i = 0; i < 10; i++)
+    beforeMount() {
+        for (let i = 0; i < 10; i++) {
         axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
-                console.log(response)
-                console.log(response.data.response)
                 this.randomEmail.push(response.data.response)
-                console.log(this.randomEmail)
             })
+        }
     },
 })
+                
+                
+                
